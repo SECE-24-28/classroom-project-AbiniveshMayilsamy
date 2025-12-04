ad56dcd490c9c60f98e826c448f475a7688d4a56
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-04 11:43:28 +0530

    concat

--- indexjs.js
+++ indexjs.js
@@ -30,56 +30,60 @@
 //   console.log("Function Expression called");
 // };
 
-// funcExp();
+// // funcExp();
 
-//----------------------
-//3.Arrow Expression
-// let arrrow = () => {
-//   console.log("Arrow Expression called");
-// };
-// arrrow();
-//---------------------
-// //4.callback function and IFFE
-// (() => {
-//   console.log("callback and IFFE function called");
-// })();
-// const addTwonumbers = (parameter1, parameter2) => {
+// //----------------------
+// //3.Arrow Expression
+// // let arrrow = () => {
+// //   console.log("Arrow Expression called");
+// // };
+// // arrrow();
+// //---------------------
+// // //4.callback function and IFFE
+// // (() => {
+// //   console.log("callback and IFFE function called");
+// // })();
+// // const addTwonumbers = (parameter1, parameter2) => {
+// //   let num1 = parameter1;
+// //   let num2 = parameter2;
+// //   let sum = num1 + num2;
+// //   console.log("The sum of two number is:", sum);
+// // };
+
+// // addTwonumbers(20, 50);
+// // addTwonumbers(40, 50);
+// // addTwonumbers();
+
+// // const subTwonumbers = (parameter1, parameter2) => {
+// //   let num1 = parameter1;
+// //   let num2 = parameter2;
+// //   let sum = num1 - num2;
+// //   console.log(`The sub of two number is:${sum}`, sum);
+// // };
+// // subTwonumbers(20, 50);
+// // subTwonumbers(40, 50);
+
+// var k = 10;
+// console.log(typeof k);
+
+// const MultiTwonumbers = (parameter1, parameter2) => {
 //   let num1 = parameter1;
 //   let num2 = parameter2;
-//   let sum = num1 + num2;
-//   console.log("The sum of two number is:", sum);
+//   let sum = num1 * num2;
+//   console.log(`The sub of two number is:${sum}`);
 // };
+// MultiTwonumbers(20, 50);
+// MultiTwonumbers(40, 50);
 
-// addTwonumbers(20, 50);
-// addTwonumbers(40, 50);
-// addTwonumbers();
-
-// const subTwonumbers = (parameter1, parameter2) => {
+// const DivTwonumbers = (parameter1, parameter2) => {
 //   let num1 = parameter1;
 //   let num2 = parameter2;
-//   let sum = num1 - num2;
-//   console.log(`The sub of two number is:${sum}`, sum);
+//   let sum = num1 / num2;
+//   console.log(`The sub of two number is:${sum}`);
 // };
-// subTwonumbers(20, 50);
-// subTwonumbers(40, 50);
+// DivTwonumbers(20, 50);
+// DivTwonumbers(40, 50);
 
-var k = 10;
-console.log(typeof k);
-
-const MultiTwonumbers = (parameter1, parameter2) => {
-  let num1 = parameter1;
-  let num2 = parameter2;
-  let sum = num1 * num2;
-  console.log(`The sub of two number is:${sum}`);
-};
-MultiTwonumbers(20, 50);
-MultiTwonumbers(40, 50);
-
-const DivTwonumbers = (parameter1, parameter2) => {
-  let num1 = parameter1;
-  let num2 = parameter2;
-  let sum = num1 / num2;
-  console.log(`The sub of two number is:${sum}`);
-};
-DivTwonumbers(20, 50);
-DivTwonumbers(40, 50);
+let a = "10";
+let b = 20;
+console.log(a + b);
