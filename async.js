bb1d8bdbda19b0f138c589a05816854361907d32
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-08 11:28:03 +0530

    Callback Hell

--- async.js
+++ async.js
@@ -2,4 +2,25 @@ setTimeout(() => {
   console.log("setTimeout Function");
 }, 2000);
 
+//callback AND timer
+
 console.log("Hello World");
+
+// callback hell
+//nested callbacks
+
+setTimeout(() => {
+  console.log("setTimeout Function 1");
+  setTimeout(() => {
+    console.log("setTimeout Function 2");
+    setTimeout(() => {
+      console.log("setTimeout Function 3");
+      setTimeout(() => {
+        console.log("setTimeout Function 4");
+        setTimeout(() => {
+          console.log("setTimeout Function 5");
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
