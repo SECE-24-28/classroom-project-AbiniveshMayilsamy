f55bf874916bab6eed5f844b16165fef619245b9
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-15 10:33:16 +0530

    *--- EXPRESS-JSON-https-ID.Writefile-update ---*

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const { STATUS_CODES } = require("http");
 const app = express();
 app.use(express.json());
 
@@ -49,19 +50,56 @@ app.get("/api/v1/abc/single/:id", (req, res) => {
   });
 });
 
-app.post("/api/v1/abc/", (req, res) => {
+app.post("/api/v1/abc/single/:id", (req, res) => {
   const id = jsonData.length;
   const newData = Object.assign({ id: id + 1 }, req.body);
   jsonData.push(newData);
   fs.writeFile("abc.json", JSON.stringify(jsonData), "utf-8", (err) => {
     if (err) {
       res.status(500).json({
-        status: "error , not to write file ",
+        status: "error",
+        message: "Internal Server Error",
+      });
+      res.status(500).json({
+        status: "error",
+        message: STATUS_CODES[500],
       });
     } else {
       res.status(201).json({
         status: "success",
         data: newData,
+        STATUS_CODES: STATUS_CODES[201],
+      });
+      //showing status code message
+      res.status(201).json({
+        status: "success",
+        data: newData,
+        STATUS_CODES: STATUS_CODES[201],
+      });
+    }
+  });
+});
+// update data
+app.put("/api/v1/abc/single/:id", (req, res) => {
+  const id = req.params.id * 1;
+  let singleData = jsonData.find((el) => el.id === id);
+  if (!singleData) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  singleData = Object.assign(singleData, req.body);
+  fs.writeFile("abc.json", JSON.stringify(jsonData), "utf-8", (err) => {
+    if (err) {
+      res.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: singleData,
       });
     }
   });
